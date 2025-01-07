e6992b70b5658f27b19dbd8c1d7b7a7157787b9d
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2025-01-08 00:16:52 +0100

    Add Doom emacs bindings

---
+++ doom-module/app/ai-agent/config.el
@@ -0,0 +1,14 @@
+;;; app/ai-agent/config.el -*- lexical-binding: t; -*-
+;;;
+(use-package! ai-agent
+  :config
+  (map!
+   :map ai-agent-mode-map
+   :localleader
+   :desc "send messages" "c" #'ai-agent-tell
+   :desc "interrupt generation" "g" #'ai-agent-interrupt
+   :desc "insert from buffer" "b" #'ai-agent-insert-buffer-contents-with-line-numbers)
+  (map!
+   :leader
+   (:prefix ("a" . "AI agent")
+    :desc "new conversation" "an" #'ai-agent-focus-new-conversation)))
---
+++ doom-module/app/ai-agent/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; app/ai-agent/packages.el
+
+(package! ai-agent
+  :recipe (:host github :repo "rhaps0dy/ai-agent.el")
+  :pin "d970b3ba14c72f11de4d72507e58f59c266d2a56")
